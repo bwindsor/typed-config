798b1b407f3be0de2d746ba043727b3be30e6a99
Jonathan Mooser <99629831+jonmooser@users.noreply.github.com>
2022-11-27 19:08:30 -0800

    Fixing black formatting

--- test/test_casts.py
+++ test/test_casts.py
@@ -14,8 +14,8 @@ def test_valid_enum_cast():
     getter = enum_cast(ExampleEnum)
     assert getter("BLUE") == ExampleEnum.BLUE
 
-   
-def test_invalid_enum_cast():   
+
+def test_invalid_enum_cast():
     getter = enum_cast(ExampleEnum)
     with pytest.raises(KeyError):
         getter("PURPLE")
